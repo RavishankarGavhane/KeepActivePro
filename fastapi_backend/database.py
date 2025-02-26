5de6cbc6ad9ed185c93c31a8deb38e04c6824668
Ravishankar Gavhane <gavhane.ravishankar4@gmail.com>
2025-02-26 16:55:45 +0530

    Added modified database.py file

--- fastapi_backend/database.py
+++ fastapi_backend/database.py
@@ -1,19 +1,14 @@
 import os
-from urllib.parse import quote
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote
 from fastapi_backend.models import Base  # Import Base from models.py
 
-# Get DATABASE_URL from environment, with fallback for local development
+# Read DATABASE_URL from environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL is None:
-    # Fallback to local configuration if DATABASE_URL is not set
-    DB_USER = "ravishankar"
-    DB_PASSWORD = quote("Ravi@1234")  # Encode special characters like '@'
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
-    DB_NAME = "KeepActivePro"
-    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set. Make sure to set it in Railway.")
 
 # Create SQLAlchemy Engine
 engine = create_engine(DATABASE_URL)
