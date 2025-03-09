9f90f0250e8e7dec14f34aea99662abc259f2043
Ravishankar Gavhane <gavhane.ravishankar4@gmail.com>
2025-03-09 19:34:49 +0530

    Deploy KeepActivePro to AWS

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,14 @@
-# fastapi_backend/models.py
 from sqlalchemy import Column, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
 
+
 Base = declarative_base()
 
 
+
+# Contact Form Table
 class ContactSubmission(Base):
     __tablename__ = "contact_submissions"
     id = Column(Integer, primary_key=True, index=True)
@@ -19,3 +21,17 @@ class ContactSubmission(Base):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.email}"
+
+
+
+# Visitor Count & Downloads Table
+class VisitorMetrics(Base):
+    __tablename__ = "visitor_metrics"
+    id = Column(Integer, primary_key=True, index=True)
+    visitor_count = Column(Integer, default=0)  # Tracks website visits
+    download_count = Column(Integer, default=0)  # Tracks downloads
+    last_updated = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __str__(self):
+        return f"Visitors: {self.visitor_count}, Downloads: {self.download_count}"
+
